dd08d51c6043bcd76d790fe44fb9ab427725553e
AleksKutsenko <alonse_97@mail.ru>
2022-09-08 13:56:58 +0300

    changes in 7th step of project

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -20,12 +20,12 @@ test('genDiff_yml', () => {
   const result = readFile('file_result.yml');
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.yml');
-  expect(gendiff(filepath1, filepath2)).toEqual(result);
+  expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(result);
 });
 
 test('genDiff_plain', () => {
   const result = readFile('plain_result.yml');
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.yml');
-  expect(gendiff(filepath1, filepath2)).toEqual(result);
+  expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
 });
